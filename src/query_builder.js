2f612b2dad973cb2bf64d5ae9ac3ecfd88d58754
Jonathan Berney <jberney@pivotal.io>
2018-02-17 12:23:47 -0800

    refactor sql joining logic

--- src/query_builder.js
+++ src/query_builder.js
@@ -5,12 +5,15 @@ const QueryBuilder = {
 };
 
 const leftJoin = ({from, foreignTable, fromKey, foreignKey}) =>
-  mysql.format(' LEFT JOIN ?? ON ??.?? = ??.??', [foreignTable, from, fromKey, foreignTable, foreignKey]);
+  mysql.format('LEFT JOIN ?? ON ??.?? = ??.??', [foreignTable, from, fromKey, foreignTable, foreignKey]);
 
 const Select = (state = {leftJoins: []}) => ({
   build: () => {
     const {from, leftJoins = []} = state;
-    return `${mysql.format('SELECT * FROM ??', [from])}${leftJoins.reduce((memo, join) => `${memo}${leftJoin({from, ...join})}`, '')}`;
+    return [
+      mysql.format('SELECT * FROM ??', [from]),
+      leftJoins.reduce((memo, join) => `${memo} ${leftJoin({from, ...join})}`, '')
+    ].join('');
   },
   from: from => Select({...state, from}),
   leftJoins: joins => Select(({...state, leftJoins: [...state.leftJoins, ...joins]}))
